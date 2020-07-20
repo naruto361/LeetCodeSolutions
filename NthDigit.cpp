545327d6b9708c9eef075d2d8a949984c0319bdd
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-20 14:54:56 +0530

    Create NthDigit.cpp

---
+++ NthDigit.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+      long long int len = 1,cnt = 9,start = 1; 
+      while(n > len * cnt){
+         n -= len * cnt;
+         cnt *= 10;
+         start *= 10;
+         len++;
+      }
+      start += (n - 1) / len;
+      string s = to_string(start);
+      return s[(n - 1) % len] - '0';
+    }
+};
