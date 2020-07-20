c36f70c17fa52dd7aa6c67f682ad23cc8f4f1b1e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-20 17:47:34 +0530

    Create FactorialTrailingZeroes.cpp

---
+++ FactorialTrailingZeroes.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        for (long long int i=5;n/i>=1;i*=5) 
+        count+=n/i; 
+        return count;
+    }
+};
