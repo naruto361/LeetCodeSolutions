049af997ef77a91122e677cb6e119e12275efd3e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-25 11:41:51 +0530

    Create NthTribonacciNumber.cpp

---
+++ NthTribonacciNumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0 || n==1) return n;
+        int f[n+1];
+        f[0]=0;f[1]=1;f[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            f[i]=f[i-1]+f[i-2]+f[i-3];
+        }
+        return f[n];
+    }
+};
