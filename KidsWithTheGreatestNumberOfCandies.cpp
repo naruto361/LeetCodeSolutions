fc0361cef1102100a4fa364c504bfec3b3fb991b
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-30 10:30:02 +0530

    Create KidsWithTheGreatestNumberOfCandies.cpp

---
+++ KidsWithTheGreatestNumberOfCandies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int k=*max_element(candies.begin(),candies.end());
+        vector<bool> v;
+        for(int i=0;i<candies.size();i++)
+        {
+            if(candies[i]+extraCandies >=k) 
+                v.push_back(1);
+            else v.push_back(0);
+        }
+        return v;
+    }
+};
