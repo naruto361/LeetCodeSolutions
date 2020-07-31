1dd79141d88f85876db91f6a5ddd99afc2479c58
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-07-31 16:10:21 +0530

    Create ShuffleString.cpp

---
+++ ShuffleString.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string a=s;
+        for(int i=0;i<indices.size();i++)
+            a[indices[i]]=s[i];
+        return a;
+    }
+};
