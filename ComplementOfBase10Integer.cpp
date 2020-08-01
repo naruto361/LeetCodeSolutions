a1b0b3f7258d5687721fa8c5a46cee87cb60545a
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-01 13:11:17 +0530

    Create ComplementOfBase10Integer.cpp

---
+++ ComplementOfBase10Integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        int b[32]; 
+        int i = 0; 
+        while (n > 0) 
+        { 
+            b[i] = n % 2; 
+            n = n / 2; 
+            i++; 
+        }
+        for(int j=i-1;j>=0;j--)
+        {
+            if(b[j]==0) b[j]=1;
+            else b[j]=0;
+        }
+        long long int ans=0,p=1;
+        for(int j=0;j<=i-1;j++)
+        {
+            ans+=b[j]*p;
+            p*=2;
+        }
+        return ans;
+    }
+};
