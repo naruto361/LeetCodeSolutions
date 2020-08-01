64db4cdbda70cc924e3818ca5034c767ee24867b
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-01 18:23:32 +0530

    Update DestinationCity.cpp

--- DestinationCity.cpp
+++ DestinationCity.cpp
@@ -7,15 +7,9 @@ public:
             mp[paths[i][0]]++;
             mp[paths[i][1]]++;
         }
-        string s[2];
-        int k=0;
-        for(auto i=mp.begin();i!=mp.end();i++)
-        {
-            if( i->second == 1) s[k++]=i->first;
-        }
         for(int i=0;i<paths.size();i++)
         {
-            if(paths[i][1]==s[0] || paths[i][1]==s[1])
+            if(mp[paths[i][1]]==1)
                 return paths[i][1];
         }
         return "0";
