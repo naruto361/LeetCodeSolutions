b8b493193649625f5dcc083ccf38e5334c004904
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-01 14:25:24 +0530

    Create TheKthFactorOfN.cpp

---
+++ TheKthFactorOfN.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> v;
+        v.push_back(1);
+        if(n!=1) v.push_back(n);
+        for(int i=2;i*i<=n;i++)
+        {   
+            if(i*i==n) 
+                v.push_back(i);
+            else if(n%i==0)
+            {v.push_back(i);v.push_back(n/i);}
+        }
+        sort(v.begin(),v.end());
+        if(k>v.size()) return -1;
+        return v[k-1];
+    }
+};
