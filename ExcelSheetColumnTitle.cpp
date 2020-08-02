296f14411c8a0d3e8d83d1e6937d04c63331c115
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-02 10:55:24 +0530

    Create ExcelSheetColumnTitle.cpp

---
+++ ExcelSheetColumnTitle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res;
+        char tmp;
+        while(n)
+        {
+            n--;
+            tmp = 'A' + n % 26;
+            res = tmp + res;
+            n /= 26;
+        }
+        return res;
+    }
+};
