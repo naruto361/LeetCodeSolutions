0133935c504791853c3ff11e7525899eb3eb7813
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-22 12:36:45 +0530

    Create SortList.cpp

---
+++ SortList.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> v;
+        ListNode *temp=head;
+        while(temp)
+        {
+            v.push_back(temp->val);
+            temp=temp->next;
+        }
+        sort(v.begin(),v.end());
+        ListNode *curr=head;
+        int k=0;
+        while(curr)
+        {
+            curr->val = v[k++];
+            curr=curr->next;
+        }
+        return head;
+        
+        
+    }
+};
