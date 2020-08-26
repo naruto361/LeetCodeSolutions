5990af9c76f4fec11950351b27911d3fec7aecc6
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-08-26 11:53:04 +0530

    Create RangeAdditionII.cpp

---
+++ RangeAdditionII.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        if(ops.size()==0) return m*n;
+        int a=INT_MAX,b=INT_MAX;
+        for(int i=0;i<ops.size();i++)
+        {
+            a=min(a,ops[i][0]);
+            b=min(b,ops[i][1]);
+        }
+        return a*b;
+    }
+};
