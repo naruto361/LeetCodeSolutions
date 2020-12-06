c634e8558a3be54ae9cb4e00a524d341c77fbdd3
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-12-06 13:21:30 +0530

    Create MinimumDistanceBetweenBSTNodes.cpp

---
+++ MinimumDistanceBetweenBSTNodes.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,int &prev,int &mini)
+    {
+        if(root==NULL) return;
+        inorder(root->left,prev,mini);
+        if(prev!=-1)
+            mini=min(root->val-prev,mini);
+        prev=root->val;
+        inorder(root->right,prev,mini);
+    }
+    int minDiffInBST(TreeNode* root) {
+        int mini=INT_MAX;
+        int prev=-1;
+        inorder(root,prev,mini);
+        return mini;
+    }
+};
