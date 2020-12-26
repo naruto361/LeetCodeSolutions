5142d5ceace1cf90e9c316aeb7d001b36a446242
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2020-12-26 10:52:41 +0530

    Create DiagonalTraverseII.cpp

---
+++ DiagonalTraverseII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+         map<int,vector<int>> v;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[i].size();j++)
+            {
+                v[i+j].push_back(matrix[i][j]);
+            }
+        }
+        vector<int> vec;
+        for(int i=0;i<v.size();i++)
+        {
+            
+                for(int j=v[i].size()-1;j>=0;j--)
+                {
+                    vec.push_back(v[i][j]);
+                }
+
+        }
+        return vec;
+    }
+};
