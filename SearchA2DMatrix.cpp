0788415a88b8293ec254fb43265957768533b85e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-02-23 19:05:46 +0530

    Update SearchA2DMatrix.cpp

--- SearchA2DMatrix.cpp
+++ SearchA2DMatrix.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-       for(int i=0;i<matrix.size();i++)
+       int m=matrix.size(),n=matrix[0].size();
+        if(m==0 || n==0) return 0;
+        int i=0;
+        while(i<m)
         {
-            for(int j=0;j<matrix[i].size();j++)
-                if(matrix[i][j]==target) return true;
+            if(matrix[i][0]>target) return 0;
+            if(matrix[i][n-1]<target) {i++;continue;}
+            if(binary_search(matrix[i].begin(),matrix[i].end(),target)==1) return 1;
+            i++;
+            if(i>=n) break;
         }
-        return false; 
+        return 0; 
     }
 };
