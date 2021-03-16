bce4867da916987de544dede60d1d48acd913faf
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-03-16 21:50:10 +0530

    Update SingleNumber2.cpp

--- SingleNumber2.cpp
+++ SingleNumber2.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        sort(nums.begin(),nums.end());
-        if(nums.size()>=3)
-        for(int i=1;i<nums.size()-1;i++)
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++) mp[nums[i]]++;
+        for(auto i=mp.begin();i!=mp.end();i++)
         {
-            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
+            if(i->second==1) return i->first;
         }
-        if(nums[nums.size()-1]!=nums[nums.size()-2]) return nums[nums.size()-1];
-        if(nums[0]!=nums[1]) return nums[0];
         return 0;
+        
     }
 };
