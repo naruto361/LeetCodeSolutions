4d01249f473ddf301f91884c40fc31f2753e53cd
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-23 08:36:06 +0530

    Update BitwiseANDofNumbersRange.cpp

--- BitwiseANDofNumbersRange.cpp
+++ BitwiseANDofNumbersRange.cpp
@@ -1,8 +1,23 @@
-class Solution {
-public:
-    int rangeBitwiseAnd(int m, int n) {
-        while(m < n) 
-            n -= (n & -n); 
-        return n;
+int msb(int t)
+    {
+        int pos=-1;
+        while(t>0)
+        {
+            t/=2;
+            pos++;
+        }
+        return pos;
     }
-};
+    int rangeBitwiseAnd(int left, int right) {
+        int ans=0;
+        while(left && right)
+        {
+            int p1=msb(left),p2=msb(right);
+            if(p1!=p2) return ans;
+            ans+=(1<<p1);
+            left-=(1<<p1);
+            right-=(1<<p1);
+            
+        }
+        return ans;  
+    }
