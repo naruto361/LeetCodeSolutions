ed94879e86e809faa280c82d93f1ee72658d12a9
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-25 12:29:18 +0530

    Update Pascal'sTriangle.cpp

--- Pascal'sTriangle.cpp
+++ Pascal'sTriangle.cpp
@@ -30,3 +30,35 @@ public:
         return v;
     }
 };
+
+********************************************
+M E T H O D  2
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v;
+        for(int i=0;i<numRows;i++)
+        {
+            if(i==0)
+            {
+                v.push_back({1});
+            }
+            else if(i==1)
+            {
+                v.push_back({1,1});
+            }
+            else
+            {
+                vector<int> t;
+                t.push_back(1);
+                for(int j=1;j<i;j++)
+                {
+                    t.push_back(v[i-1][j-1]+v[i-1][j]);
+                    
+                }
+                t.push_back(1);
+                v.push_back(t);
+                t.clear();
+            }
+        }
+        return v;
+    }
+    
