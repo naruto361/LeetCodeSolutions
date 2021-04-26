4c94a06eee02fa4b0d95c78337241b563f355b6e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-26 09:41:24 +0530

    Create DecodeXORedArray.cpp

---
+++ DecodeXORedArray.cpp
@@ -0,0 +1,9 @@
+vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> v;
+        v.push_back(first);
+        for(int i=0;i<encoded.size();i++)
+        {
+            v.push_back(v[v.size()-1]^encoded[i]);
+        }
+        return v;
+    }
