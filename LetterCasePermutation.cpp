b82b50c064f2c6634f8e8456532d1bdf651f6c34
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-26 10:42:18 +0530

    Just like subsequence using bits
    if character is a digit then print digit without modification
    else ......
    if bit is 1 then print letter in upper case
    else print letter in lower case

--- LetterCasePermutation.cpp
+++ LetterCasePermutation.cpp
@@ -6,15 +6,17 @@ vector<string> letterCasePermutation(string S) {
             string s;
             for(int j=0;j<S.length();j++)
             {   
+                    \\ if digit add to string as it is
                 if(S[j]>='0' && S[j]<='9') s+=S[j];
                 else
                 {
+                        \\ if bit is 1 add uppercase of the character
+                        \\   else lowercase of the character
                     if(i>>j & 1) s+=toupper(S[j]);
                     else s+=tolower(S[j]);
                 }
             }
             c.insert(s);
-            
         }
         for(auto i:c) v.push_back(i);
         return v;
