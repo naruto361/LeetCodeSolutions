6d9913379aeeaaa9779b0d0b2feef1c683b44f46
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-27 12:55:38 +0530

    Update MaximumSubarray.cpp

--- MaximumSubarray.cpp
+++ MaximumSubarray.cpp
@@ -1,13 +1,9 @@
-class Solution {
-public:
-    int maxSubArray(vector<int>& nums) {
-        int best=-999999,sum=-999999;
-        if(nums.size()==1)  return nums[0];
-        for(int i=0;i<nums.size();i++)
+int maxSubArray(vector<int>& nums) {
+        int ans=nums[0],best=nums[0];
+        for(int i=1;i<nums.size();i++)
         {
-            sum=max( nums[i] , (nums[i]+sum) );
-            best=max(best,sum);
+            ans=max(nums[i]+ans,nums[i]);
+            best=max(ans,best);
         }
         return best;
     }
-};
