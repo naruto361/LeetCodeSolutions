9f6b70a62689f0063dd0268c5748bc47e93c9010
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-07 13:20:49 +0530

    Create GenerateParentheses.cpp

---
+++ GenerateParentheses.cpp
@@ -0,0 +1,25 @@
+vector<string> v;
+    void check(string &s,int open,int close)
+    {
+        if(open==0 && close==0) {v.push_back(s);return;}
+        if(open>0)
+        {
+            s.push_back('(');
+            check(s,open-1,close);
+            s.pop_back();
+        }
+        if(close>0)
+        {
+            if(open<close)
+            {
+                s.push_back(')');
+                check(s,open,close-1);
+                s.pop_back();
+            }
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string s;
+        check(s,n,n);
+        return v;
+    }
