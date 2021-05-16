ad57ca230622dee2a8bd41bb80ee7428959b66f4
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-16 09:30:29 +0530

    ***************

---
+++ coinchange2.cpp
@@ -0,0 +1,9 @@
+int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount+1,0);
+        dp[0]=1;
+        for(auto c:coins)
+        {
+            for(auto j=c;j<=amount;j++) dp[j]+=dp[j-c];
+        }
+        return dp[amount];
+    }
