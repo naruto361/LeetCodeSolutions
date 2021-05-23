5d57d2279ecb8bc919617948eb34c73e5d0d108c
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-23 13:16:20 +0530

    Update MaximumProductOfTwoElementsInAnArray.cpp

--- MaximumProductOfTwoElementsInAnArray.cpp
+++ MaximumProductOfTwoElementsInAnArray.cpp
@@ -1,7 +1,10 @@
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),greater<int>());
-        return (nums[0]-1)*(nums[1]-1);
+int maxProduct(vector<int>& nums) {
+        int max1=0,max2=0;
+        for(int i:nums)
+        {
+            if(i>max1) max2=max1,max1=i;
+            else if(i>max2) max2=i;
+        }
+        //cout<<max1<<" "<<max2;
+        return (max1-1)*(max2-1);
     }
-};
