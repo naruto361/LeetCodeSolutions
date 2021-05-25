5283589dba5c66462846872ce914c364e36b379f
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-25 13:09:04 +0530

    Update EvaluateReversePolishNotation.cpp

--- EvaluateReversePolishNotation.cpp
+++ EvaluateReversePolishNotation.cpp
@@ -1,51 +1,39 @@
-class Solution {
-public:
+int oper(string s)
+    {
+        if(s=="+") return 1;
+        else if(s=="-") return 2;
+        else if(s=="*") return 3;
+        else if(s=="/") return 4;
+        return 100;
+    }
     int evalRPN(vector<string>& tokens) {
-        stack <int> s;
-        int ans=0;
+        stack<int> st;
         for(int i=0;i<tokens.size();i++)
         {
-            if(tokens[i]=="+")
+            int z=oper(tokens[i]);
+            if(z<=4)
             {
-                int a=s.top();
-                s.pop();
-                int b=s.top();
-                s.pop();
-                ans=b+a;
-                s.push(ans);
+                int k=st.top();
+                st.pop();
+                if(z==1)
+                {
+                    st.top()=st.top()+k;
+                }
+                else if(z==2)
+                {
+                    st.top()=st.top()-k;
+                }
+                else if(z==3)
+                {
+                    st.top()=st.top()*k;
+                }
+                else if(z==4)
+                {
+                    st.top()=st.top()/k;
+                }
+                
             }
-           else if(tokens[i]=="-")
-            {
-                int a=s.top();
-                s.pop();
-                int b=s.top();
-                s.pop();
-                ans=b-a;
-               s.push(ans);
-            }
-            else if(tokens[i]=="*")
-            {
-                int a=s.top();
-                s.pop();
-                int b=s.top();
-                s.pop();
-                ans=b*a;
-                s.push(ans);
-            }
-            else if(tokens[i]=="/")
-            {
-                int a=s.top();
-                s.pop();
-                int b=s.top();
-                s.pop();
-                ans=b/a;
-                s.push(ans);
-            }
-            else
-                s.push(stoi(tokens[i]));
+            else st.push(stoi(tokens[i]));
         }
-        return s.top();
-            
-            
-        }
-};
+        return st.top();
+    }
