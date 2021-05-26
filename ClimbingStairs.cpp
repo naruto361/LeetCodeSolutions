86bf6fab1f0e1c4396039eb7d597b6493c26e5b4
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-26 08:37:13 +0530

    Update ClimbingStairs.cpp

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -1,13 +1,11 @@
-class Solution {
-public:
-    int climbStairs(int n) {
-        int f[n+1];
-        if(n==1) return 1;
-        if(n==2) return 2;
-        f[1]=1;
-        f[2]=2;
-        for(int i=3;i<=n;i++)
-            f[i]=f[i-1]+f[i-2];
-        return f[n];
+int climbStairs(int n) {
+        if(n<=3) return n;
+        int f=3,s=2;
+        for(int i=4;i<=n;i++)
+        {
+            int k=f;
+            f=f+s;
+            s=k;
+        }
+        return f;
     }
-};
