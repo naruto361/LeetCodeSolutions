75ca01365088b02d7d8744c75cf51de1ac052f08
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-27 17:02:31 +0530

    string::npos

---
+++ MaximumRepeatingSubstring.cpp
@@ -0,0 +1,10 @@
+int maxRepeating(string sequence, string word) {
+        int count=0;
+        string temp=word;
+        while(sequence.find(temp)!=string::npos)
+        {
+            count++;
+            temp+=word;
+        }
+        return count;
+    }
