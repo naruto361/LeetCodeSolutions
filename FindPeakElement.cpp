01ba54414ce6e0ab1f93de9ef400e8f284f7bd0e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-05-28 19:30:38 +0530

    Update FindPeakElement.cpp

--- FindPeakElement.cpp
+++ FindPeakElement.cpp
@@ -1,14 +1,15 @@
-class Solution {
-public:
-    int findPeakElement(vector<int>& nums) {
+int findPeakElement(vector<int>& nums) {
         if(nums.size()==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[nums.size()-1]>nums[nums.size()-2]) return nums.size()-1;
+        if(nums.size()==2)
+        {
+            if(nums[1]>nums[0]) return 1;
+        }
         for(int i=1;i<nums.size()-1;i++)
         {
             if(nums[i]>nums[i-1] && nums[i]>nums[i+1]) return i;
         }
-        int maxi=*max_element(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++)
-            if(nums[i]==maxi) return i;
+        
         return 0;
     }
-};
