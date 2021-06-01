a31181c4fbcecb903ce5257962a8840c3a29755c
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-01 11:23:43 +0530

    *** imp ***

---
+++ JumpGame3.cpp
@@ -0,0 +1,9 @@
+bool canReach(vector<int>& arr, int start) {
+        if(start>=0 && start<arr.size() && arr[start]>=0 && arr[start]<arr.size())
+        {
+            arr[start]=-arr[start];
+            return arr[start]==0 || 
+                (canReach(arr,start+arr[start]) || canReach(arr,start-arr[start]));
+        }
+        return false;
+    }
