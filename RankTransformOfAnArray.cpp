3787645948511ff6cfde910cf41b6c7a268ccd69
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-03 00:18:06 +0530

    Update RankTransformOfAnArray.cpp

--- RankTransformOfAnArray.cpp
+++ RankTransformOfAnArray.cpp
@@ -1,5 +1,4 @@
-class Solution {
-public:
+
     vector<int> arrayRankTransform(vector<int>& arr) {
         map<int,int> m;
         for(int i=0;i<arr.size();i++)
@@ -13,4 +12,26 @@ public:
         }
         return arr;
     }
-};
+
+######################################
+
+vector<int> arrayRankTransform(vector<int>& arr) {
+        if(arr.size()==0) return {};
+        vector<int> v=arr;
+        sort(v.begin(),v.end());
+        unordered_map<int,int> mp;
+        mp[v[0]]=1;
+        int k=1;
+        for(int i=1;i<v.size();i++)
+        {
+            if(v[i]!=v[i-1])
+            {
+                mp[v[i]]=++k;
+            }
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            arr[i]=mp[arr[i]];
+        }
+        return arr;
+    }
