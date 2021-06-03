23e465a0e6e40dafdc9db3c9c4c926b4b0a08a77
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-03 14:47:23 +0530

    moore majority vote algo

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,17 +1,16 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
-        sort(nums.begin(),nums.end());
-        int best=1,ans=1;
-        int ok=1;
+// MOORE MAJORITY VOTE ALGO
+int majorityElement(vector<int>& nums) {
+        int cnt=0,maxi=0;
+        maxi=nums[0];
+        cnt++;
         for(int i=1;i<nums.size();i++)
         {
-            if(nums[i]==nums[i-1]) {ans++;best=max(ans,best);if(best==ans) ok=nums[i];}
+            if(nums[i]==maxi) cnt++,maxi=nums[i];
             else
-            {ans=1;
+            {
+                cnt--;
+                if(cnt==0) maxi=nums[i],cnt=1;
             }
         }
-        return ok;
+        return maxi;
     }
-};
