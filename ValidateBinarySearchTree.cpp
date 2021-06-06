0a1f7c0553192bd5568919af748f7ce240e7e630
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-06 23:53:19 +0530

    Update ValidateBinarySearchTree.cpp

--- ValidateBinarySearchTree.cpp
+++ ValidateBinarySearchTree.cpp
@@ -1,30 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void inorder(vector<int> &v,TreeNode* root)
+long long m=-1e10;
+    int f=true;
+    void ok(TreeNode* root)
     {
-        if(root==NULL) return;
-        inorder(v,root->left);
-        v.push_back(root->val);
-        inorder(v,root->right);
+        if(root==NULL) return ;
+        ok(root->left);
+        if(root->val<=m) {f=false;return;}
+        m=root->val;
+        ok(root->right);
     }
     bool isValidBST(TreeNode* root) {
-        vector<int> v;
-        inorder(v,root);
-        for(int i=0;i<v.size()-1;i++)
-        {
-            if(v[i]>=v[i+1]) return 0;
-        }
-        return 1;
+        if(root==NULL) return 1;
+        if(!root->left && !root->right) return 1;
+        ok(root);
+        return f;
     }
-};
