00e8d788ac891826e5a891758606ef78d9502570
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-08 22:47:02 +0530

    Update ConvertBSTtoGreaterTree.cpp

--- ConvertBSTtoGreaterTree.cpp
+++ ConvertBSTtoGreaterTree.cpp
@@ -1,28 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    void change(TreeNode* &root,int &sum)
+void check(TreeNode* &root,int &sum)
     {
-        if(root==NULL) return;
-        change(root->right,sum);
-        sum+=root->val;
-        root->val=sum;
-        change(root->left,sum);
+        if(root==NULL) return ;
+        check(root->right,sum);
+        root->val+=sum;
+        sum=root->val;
+        check(root->left,sum);
     }
     TreeNode* convertBST(TreeNode* root) {
         int sum=0;
-        
-        change(root,sum);
+        check(root,sum);
         return root;
     }
-};
