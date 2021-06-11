a55cc763a8949812cb53b8a169c65209bd5d4196
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-11 17:44:07 +0530

    Create StoneGame.cpp

---
+++ StoneGame.cpp
@@ -0,0 +1,8 @@
+bool stoneGame(vector<int>& piles) {
+        // if(piles.size()==2) return true;
+        // int sum1=0,sum2=0;
+        // for(int i=0;i<piles.size();i+=2) sum1+=piles[i];
+        // for(int i=1;i<piles.size();i+=2) sum2+=piles[i]; 
+        // return true;
+        return true;
+    }
