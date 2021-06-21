1cd45ca51ae905719271b2aa648f5dd8eaa08013
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-06-21 13:51:41 +0530

    Update PascalsTriangle2.cpp

--- PascalsTriangle2.cpp
+++ PascalsTriangle2.cpp
@@ -26,3 +26,38 @@ vector<vector<int>> generate(int numRows) {
         }
         return v;
     }
+
+/*************** M E T H O D  -  2 *******************/
+ class Solution {
+private:
+     int solve(int n,int k)
+     {
+        long long res = 1; 
+        if (k > n - k) 
+        k = n - k; 
+        for (int i = 0; i < k; ++i) 
+        { 
+            res *= (n - i); 
+            res /= (i + 1); 
+        } 
+        return (int)res;
+     }
+public:
+    vector<int> getRow(int rowIndex) {
+        if(rowIndex==0) return {1};
+        else if(rowIndex==1) return {1,1};
+        vector<int> v;
+        v.push_back(1);
+        for(int i=1;i<=rowIndex/2;i++)
+        {
+            v.push_back(solve(rowIndex,i));
+        }
+        int l=v.size();
+        if(rowIndex&1) v.push_back(v[l-1]);
+        for(int i=l-2;i>=0;i--)
+        {
+            v.push_back(v[i]);
+        }
+        return v;
+    }
+};
