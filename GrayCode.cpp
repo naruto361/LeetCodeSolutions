d7be4a620e91708a12875f0fc1a25cd8b04127da
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-01 16:34:52 +0530

    2 methods

--- GrayCode.cpp
+++ GrayCode.cpp
@@ -10,3 +10,37 @@ public:
         return ans;
     }
 };
+/*******************************************************/
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> v;
+        v.push_back(0);
+        for(int i=1;i<(1<<n);i++)
+        {
+            vector<int> t;
+            int k=i;
+            while(k)
+            {
+                t.push_back(k%2);
+                k/=2;
+            }
+            reverse(t.begin(),t.end());
+            int ans=0;
+            int p=t.size()-1;
+            ans|=(1<<p);
+            p--;
+            for(int j=1;j<t.size();j++)
+            {
+                if(t[j]^t[j-1]==1)
+                {
+                    ans|=(1<<p);
+                }
+                p--;
+            }
+            //cout<<ans<<" ";
+            v.push_back(ans);
+        }
+        return v;
+    }
+};
