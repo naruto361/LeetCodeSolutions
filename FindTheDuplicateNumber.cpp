391645dd4b25ec089ec9c59f4ca0175163c8af77
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-03 08:41:31 +0530

    Update FindTheDuplicateNumber.cpp

--- FindTheDuplicateNumber.cpp
+++ FindTheDuplicateNumber.cpp
@@ -9,3 +9,23 @@ public:
         return 0;
     }
 };
+/******************** O(1) Extra Space *****************************/
+int findDuplicate(vector<int>& nums) {
+        int slow=nums[0];
+        int fast=nums[0];
+        int en=nums[0];
+        do
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            //cout<<slow<<" "<<fast<<'\n';
+        }while(slow!=fast);
+        //cout<<"ok: "<<slow<<" "<<fast<<'\n';
+        while(slow!=en)
+        {
+            slow=nums[slow];
+            en=nums[en];
+           // cout<<slow<<" "<<en<<'\n';
+        }
+        return slow;
+    }
