9af70648ebecb144e98a5cca02e6d9476023d52e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-04 09:48:46 +0530

    Update LongestIncreasingSubsequence.cpp

--- LongestIncreasingSubsequence.cpp
+++ LongestIncreasingSubsequence.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& v) {
-        if (v.size() == 0)  
-        return 0;  
-        vector<int> tail(v.size(), 0);  
-        int length = 1; 
-        tail[0] = v[0];  
-        for (int i = 1; i < v.size(); i++) 
-        {  
-            auto b = tail.begin(), e = tail.begin() + length; 
-            auto it = lower_bound(b, e, v[i]);  
-            if (it == tail.begin() + length) 
-            tail[length++] = v[i];  
-            else   
-            *it = v[i];  
-        }  
-    return length; 
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size()==1) return 1;
+        vector<int> ans(nums.size(),1);
+        int idx=0;
+        int maxi=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    ans[i]=max(ans[i],1+ans[j]);
+                }
+            }
+            if(ans[i]>maxi)
+            {
+                idx=i;
+                maxi=ans[i];
+            }
+        }
+        cout<<nums[idx]<<" ";;
+        int j=idx;
+        for(int i=idx-1;i>=0;i--)
+        {
+            if(ans[i]+1==ans[j]) 
+            {
+                cout<<nums[i]<<" ";
+                j=i;
+            }
+        }
+        return maxi;
     }
 };
+/********************************* METHOD 2 *************************/
+int lengthOfLIS(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            auto k=lower_bound(ans.begin(),ans.end(),nums[i]);
+            if(k==ans.end()) ans.push_back(nums[i]);
+            else *k=nums[i];
+        }
+        return ans.size();
+    }
