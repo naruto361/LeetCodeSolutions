a3cb5e86315e46635f6ba1128e66de1f50856488
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-06 08:23:25 +0530

    Update PlusOne.cpp

--- PlusOne.cpp
+++ PlusOne.cpp
@@ -14,3 +14,14 @@ vector<int> plusOne(vector<int>& digits) {
         reverse(digits.begin(),digits.end());
         return digits;
     }
+/*******************************************/
+        int carry=1;
+        int n=digits.size();
+            for(int i=n-1;i>=0;i--)
+            {
+                digits[i]+=carry;
+                carry=digits[i]/10;
+                digits[i]=digits[i]%10;
+            }
+        if(carry!=0) digits.insert(digits.begin(),carry);
+        return digits;
