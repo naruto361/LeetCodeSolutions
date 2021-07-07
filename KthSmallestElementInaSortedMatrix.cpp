f52bcc068fd17314ab1143fdd8f7ff23ec8c118c
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-07 17:04:52 +0530

    Update KthSmallestElementInaSortedMatrix.cpp

--- KthSmallestElementInaSortedMatrix.cpp
+++ KthSmallestElementInaSortedMatrix.cpp
@@ -13,3 +13,20 @@ public:
         return ans[k-1];
     }
 };
+/********************************************/
+int kthSmallest(vector<vector<int>>& matrix, int k) {
+        priority_queue<int> pq;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(pq.size()<k) pq.push(matrix[i][j]);
+                else if(matrix[i][j]<pq.top())
+                {
+                    pq.pop();
+                    pq.push(matrix[i][j]);
+                }
+            }
+        }
+        return pq.top();
+    }
