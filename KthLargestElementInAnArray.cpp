906985600705da3e1f90b950477905d4ccc48ce7
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-08 15:34:24 +0530

    Update KthLargestElementInAnArray.cpp

--- KthLargestElementInAnArray.cpp
+++ KthLargestElementInAnArray.cpp
@@ -5,3 +5,20 @@ public:
         return nums[k-1];
     }
 };
+/****************************************************/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int &i:nums)
+        {
+            if(pq.size()<k) pq.push(i);
+            else if(i>pq.top()) 
+            {
+                pq.pop();
+                pq.push(i);
+            }
+        }
+        return pq.top();
+    }
+};
