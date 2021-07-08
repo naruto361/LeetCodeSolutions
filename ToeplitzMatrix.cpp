75a17894a02e79cf345c4707f9facd29c6ce0e0a
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-08 23:47:13 +0530

    Update ToeplitzMatrix.cpp

--- ToeplitzMatrix.cpp
+++ ToeplitzMatrix.cpp
@@ -21,3 +21,17 @@ public:
         return true;
     }
 };
+/******************************************/
+bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mp.count(i-j)==0) mp[i-j]=matrix[i][j];
+                else if(matrix[i][j]!=mp[i-j]) return false;
+            }
+        }
+        return true;
+    }
