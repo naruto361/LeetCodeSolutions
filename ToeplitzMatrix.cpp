e515e44032b5015fefc80c6ca48df38d4a240ca2
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-08 23:53:50 +0530

    Update ToeplitzMatrix.cpp

--- ToeplitzMatrix.cpp
+++ ToeplitzMatrix.cpp
@@ -1,3 +1,4 @@
+1.map of int and vector 
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
@@ -22,6 +23,7 @@ public:
     }
 };
 /******************************************/
+2.map of int ,int 
 bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         int m=matrix.size(),n=matrix[0].size();
         unordered_map<int,int> mp;
@@ -31,6 +33,21 @@ bool isToeplitzMatrix(vector<vector<int>>& matrix) {
             {
                 if(mp.count(i-j)==0) mp[i-j]=matrix[i][j];
                 else if(matrix[i][j]!=mp[i-j]) return false;
+            }
+        }
+        return true;
+    }
+/*************************************************************************/
+3.array 
+bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        vector<int> v(41,-1);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(v[i-j+20]==-1) v[i-j+20]=matrix[i][j];
+                else if(matrix[i][j]!=v[i-j+20]) return false;
             }
         }
         return true;
