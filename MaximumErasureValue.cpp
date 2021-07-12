36e90f30c2e11238686bb8257c3b43dd1f0167f1
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-12 11:22:59 +0530

    Update MaximumErasureValue.cpp

--- MaximumErasureValue.cpp
+++ MaximumErasureValue.cpp
@@ -1,3 +1,4 @@
+O(N) O(1)
 int maximumUniqueSubarray(vector<int>& nums) {
         int n = nums.size(), sum = 0, maxi = 0, l = 0, r = 0;
         bool freq[10001]{false};
@@ -10,3 +11,19 @@ int maximumUniqueSubarray(vector<int>& nums) {
         }
         return maxi;
     }
+/********************************************/
+O(N) O(N)
+        class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n = size(nums), cur_sum = 0, ans = 0, l = 0, r = 0;
+        unordered_set<int> s;
+        while(r < n) {
+            while(s.find(nums[r]) != end(s))
+                cur_sum -= nums[l], s.erase(nums[l++]);
+            cur_sum += nums[r], s.insert(nums[r++]);
+            ans = max(ans, cur_sum);
+        }
+        return ans;
+    }
+};
