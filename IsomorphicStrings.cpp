50c8d728eac73f63be35fc119f9ef2e5ab6d0eb7
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-12 13:40:36 +0530

    Update IsomorphicStrings.cpp

--- IsomorphicStrings.cpp
+++ IsomorphicStrings.cpp
@@ -1,3 +1,21 @@
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!= t.length()) return false;
+        map<char,char> mp1,mp2;
+        for(int i=0;i<s.length();i++)
+        {
+            if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end()) 
+                mp1[s[i]]=t[i],mp2[t[i]]=s[i];
+            else if(mp1.find(s[i])==mp1.end() ^ mp2.find(t[i])==mp2.end())
+                return false;
+            else if(mp1[s[i]]!=t[i] && mp2[t[i]]!=s[i]) return false;
+        }
+        return true;
+    }
+};
+/**************************************************************/
 class Solution {
 public:
     bool isIsomorphic(string str1, string str2) {
