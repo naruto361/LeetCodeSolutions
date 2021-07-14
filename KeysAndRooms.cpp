358789554c8a7f3373f353b0683fbed73bd4d16a
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-07-14 11:40:47 +0530

    connected components to be 1

--- KeysAndRooms.cpp
+++ KeysAndRooms.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     // connected components : 1
-    void dfs(vector<vector<int>> &a,vector<bool> &vis,int node)
+    void dfs(vector<vector<int>> &rooms,vector<bool> &vis,int node)
     {
         vis[node]=1;
         //cout<<node<<" ";
-        for(auto &child:a[node])
+        for(auto &child:rooms[node])
         {
             if(!vis[child])
             {
-                dfs(a,vis,child);
+                dfs(rooms,vis,child);
             }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<vector<int>> a(rooms.size());
         vector<bool>vis(rooms.size(),false);
-        for(int i=0;i<rooms.size();i++)
-        {
-            for(int j=0;j<rooms[i].size();j++) 
-                a[i].push_back(rooms[i][j]);
-        }
         int ans=0;
         for(int i=0;i<rooms.size();i++)
         {
             if(!vis[i])
             {
                 ans++;
-                dfs(a,vis,i);
+                dfs(rooms,vis,i);
                 cout<<"\n";
             }
         }
