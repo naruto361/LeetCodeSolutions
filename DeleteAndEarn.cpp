64b0b0356a45b3528e102c30163d18b2309ce5c1
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-10-22 13:41:30 +0530

    Update DeleteAndEarn.cpp

--- DeleteAndEarn.cpp
+++ DeleteAndEarn.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
+        int f[10001]={0};
+        for(int &i:nums) f[i]++;
+        for(int i=1;i<=10000;i++) f[i]*=i;
+        for(int i=2;i<=10000;i++)
+        {
+            f[i]=max(f[i-1],f[i]+f[i-2]);
+        }
+        return f[10000];
+    }
+};
+
+
+//////////////////////////////////////////
+
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
         int maxi=0;
         unordered_map<int,int> mp;
         for(int &i:nums)
