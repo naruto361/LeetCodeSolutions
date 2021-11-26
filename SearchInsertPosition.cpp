5744fef354208882f347e3af75561624ed36903a
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-11-26 10:34:07 +0530

    Update SearchInsertPosition.cpp

--- SearchInsertPosition.cpp
+++ SearchInsertPosition.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>=target) return i;
-        }      
-        return nums.size();
+        int l=0,h=nums.size()-1;
+        if(target<nums[0]) return 0;
+        if(target>nums[h]) return h+1;
+        auto it = lower_bound(nums.begin(),nums.end(),target);
+        return it-nums.begin();
+        // while(l<h)
+        // {
+        //     int mid = l + (h-l)/2;
+        //     if(nums[mid]>=target) h=mid;
+        //     else l=mid+1;
+        // }
+        //return h;
     }
 };
