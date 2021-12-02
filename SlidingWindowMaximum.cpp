56ca3e17cc4a1bdb9d7823ed26664005c02ef46b
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-02 17:39:20 +0530

    2 methods

--- SlidingWindowMaximum.cpp
+++ SlidingWindowMaximum.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // if(k==1) return nums;
-        // else if(k==nums.size()) return {*max_element(nums.begin(),nums.end())};
-        // multiset<int> s;
-        // vector<int> ans;
-        // for(int i=0;i<k;i++) s.insert(nums[i]);
-        // ans.push_back(*(next( s.begin(), k-1 )));
-        // for(int i=k;i<nums.size();i++)
-        // {
-        //     s.insert(nums[i]);
-        //     s.erase(s.find(nums[i-k]));
-        //     ans.push_back(*(next( s.begin(), k-1 )));
-        // }
-        // return ans;
-        // deque<int> dq;
-        // vector<int> ans;
-        // for (int i=0; i<nums.size(); i++) {
-        //     if (!dq.empty() && dq.front() == i-k) dq.pop_front();
-        //     while (!dq.empty() && nums[dq.back()] < nums[i])
-        //         dq.pop_back();
-        //     dq.push_back(i);
-        //     if (i>=k-1) ans.push_back(nums[dq.front()]);
-        // }
-        // return ans;
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++)
@@ -34,5 +11,39 @@ public:
             if(i>=k-1) ans.push_back(nums[dq.front()]);
         }
         return ans;
+    }
+};
+//////////////////////////////////////////////////////////
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k==1) return nums;
+        if(k==nums.size()) return {*max_element(nums.begin(),nums.end())};
+        int n=nums.size();
+        vector<int> v(n,nums.size());
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && nums[i]>nums[st.top()])
+            {
+                v[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        vector<int> maxi(n-k+1);
+        int j=0;
+        for(int i=0;i<=n-k;i++)
+        {
+            int l=i+k;
+            int temp;
+            if(j<i) j=i;
+            while(v[j]<l)
+            {
+                j=v[j];
+            }
+            maxi[i]=nums[j];
+        }
+        return maxi;
     }
 };
