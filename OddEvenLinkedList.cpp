d5874e38ea7d4e9519a07dcc9e11fa59699132ae
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-02 13:58:05 +0530

    Update OddEvenLinkedList.cpp

--- OddEvenLinkedList.cpp
+++ OddEvenLinkedList.cpp
@@ -24,3 +24,34 @@ public:
         return head;
     }
 };
+//////////////////////////////////////////////////////////////////////
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) return NULL;
+        if(!head->next) return head;
+        ListNode* start=NULL;
+        ListNode* end=NULL;
+        start=end=new ListNode(head->val);
+        ListNode* t=head->next->next;
+        while(t)
+        {
+            end->next=new ListNode(t->val);
+            end=end->next;
+            if(t->next)
+                t=t->next->next;
+            else break;
+        }
+        t=head;
+        t=t->next;
+        while(t)
+        {
+            end->next=new ListNode(t->val);
+            end=end->next;
+            if(t->next)
+                t=t->next->next;
+            else break;
+        }
+        return start;
+    }
+};
