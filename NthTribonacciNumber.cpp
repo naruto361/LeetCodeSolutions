cd8c280a410aa24433a74d46977a49d393231432
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-02 14:47:28 +0530

    Update NthTribonacciNumber.cpp

--- NthTribonacciNumber.cpp
+++ NthTribonacciNumber.cpp
@@ -11,3 +11,20 @@ public:
         return f[n];
     }
 };
+/////////////////////////////////////////////////////
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n<2) return n;
+        if(n==2) return 1;
+        int a=0,b=1,c=1;
+        for(int i=3;i<n;i++)
+        {
+            int t=c;
+            c+=a+b;
+            a=b;
+            b=t;
+        }
+        return a+b+c;
+    }
+};
