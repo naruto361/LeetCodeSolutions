b85cd233f3cb8a288ecd6e0c23e5ba07a9c9585f
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-12 13:25:14 +0530

    96

--- UniqueBST.cpp
+++ UniqueBST.cpp
@@ -1,3 +1,27 @@
+class Solution {
+public:
+    long long binomialCoeff(long long n, long long k) 
+    { 
+        long long res = 1; 
+        if (k > n - k) 
+        k = n - k; 
+        long long i;
+        for (i = 0; i < k; ++i) 
+        { 
+            res *= (n - i); 
+            res /= (i + 1); 
+        } 
+
+        return res; 
+    } 
+    int numTrees(int n) {
+        long long ans = binomialCoeff(1LL*2*n,1LL*n);
+        return ans/(n+1);
+    }
+};
+
+//////////////////////////////////////////////////////////
+
 int numTrees(int n) {
         if(n<3) return n;
         vector<int> dp(n+1);
