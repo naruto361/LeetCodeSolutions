9f01904705911b1a0d6e13c799c016d9c023a02e
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-13 11:56:52 +0530

    112

--- PathSum.cpp
+++ PathSum.cpp
@@ -1,10 +1,19 @@
-bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root==NULL) return 0;
-        else if(root->right==NULL && root->left==NULL && targetSum-root->val==0)
-        {
-            return 1;
-        }
-        else
-            return hasPathSum(root->left,targetSum - root->val) || 
-            hasPathSum(root->right,targetSum - root->val);
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        if(!root->left && !root->right && root->val==targetSum) return true;
+        return (hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val));
     }
+};
