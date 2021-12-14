c003f4539485edd707b5291a7f216ec3c92740f0
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-14 09:10:17 +0530

    234

--- PalindromeLinkedList.cpp
+++ PalindromeLinkedList.cpp
@@ -10,6 +10,59 @@
  */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head)
+    {
+        ListNode* curr=head;
+        ListNode *next=NULL,*prev=NULL;
+        if(!curr || !curr->next) return head;
+        while(curr)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+        // head=prev;
+        // return head;
+        
+    }
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next) return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next && fast->next->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        ListNode* temp=head;
+        while(slow)
+        {
+            if(temp->val != slow->val) return false;
+            slow=slow->next;
+            temp=temp->next;
+        }
+        return true;
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
     bool isPalindrome(ListNode* head) {
         ListNode *curr=head;
         stack <int> s;
