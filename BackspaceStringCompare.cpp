13c0ba0be9de24cff0b11ea912fdfd85e7e9c0ca
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-25 11:38:20 +0530

    Update BackspaceStringCompare.cpp

--- BackspaceStringCompare.cpp
+++ BackspaceStringCompare.cpp
@@ -19,3 +19,32 @@ public:
         return s==t;
     }
 };
+////////////////////////////////////////////////////////
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> st1,st2;
+        for(char c:s)
+        {
+            if(c!='#') st1.push(c);
+            else {
+                if(!st1.empty()) st1.pop();
+            }
+        }
+        for(char c:t)
+        {
+            if(c!='#') st2.push(c);
+            else {
+                if(!st2.empty()) st2.pop();
+            }
+        }
+        if(st1.size() != st2.size()) return false;
+        while(!st1.empty() && !st2.empty())
+        {
+            if(st1.top()!=st2.top()) return false;
+            st1.pop();
+            st2.pop();
+        } 
+        return true;
+    }
+};
