15ae1a5816bbd77bc6e014d64c0c88cae2f4a4ff
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-12-25 11:56:53 +0530

    240

--- SearchA2DMatrixII.cpp
+++ SearchA2DMatrixII.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        if (m == 0) return false;
-        int n = matrix[0].size();
-        if(n==0) return false;
-        int i = 0, j = n - 1;
-        while (i < m && j >= 0) {
-            if (matrix[i][j] == target)
-                return true;
-            else if (matrix[i][j] > target) {
-                j--;
-            } else 
-                i++;
+        int m=matrix.size() , n=matrix[0].size();
+        // for(int i=0;i<m;i++)
+        // {
+        //     if(matrix[i][0]>target) return false;
+        //     if(matrix[i][n-1]<target) continue;
+        //     if(binary_search(matrix[i].begin(),matrix[i].end(),target)) 
+        //         return true;
+        // }
+        // return false;
+        int i=0,j=n-1;
+        while(i<m && j>=0)
+        {
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]>target) j--;
+            else i++;
         }
         return false;
     }
 };
-
-/////////////////////////////////////////
-        int m=matrix.size() , n=matrix[0].size();
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[i][0]>target) return false;
-            if(matrix[i][n-1]<target) continue;
-            if(binary_search(matrix[i].begin(),matrix[i].end(),target)) return true;
-        }
-        return false;
