da4f1a54dcc342f9e1680fdf144acc37c5f124c7
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2022-01-06 18:49:10 +0530

    1013

--- PartitionArrayIntoThreePartsWithEqualSum.cpp
+++ PartitionArrayIntoThreePartsWithEqualSum.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    bool canThreePartsEqualSum(vector<int>& A) {
-        int total=0;
-        for(int i=0;i<A.size();i++) total+=A[i];
-        if(total%3!=0)    return 0;
-        int sum=0,parts=0;
-        for (int i = 0; i < A.size(); i++) 
+    bool canThreePartsEqualSum(vector<int>& arr) {
+        int sum=0;
+        for(int &i:arr) sum+=i;
+        if(sum%3!=0) return false;
+        int k=sum/3;
+        bool ok1=false,ok2=false,ok3=false;
+        int temp=0;
+        for(auto &i:arr)
         {
-            sum += A[i];
-            if (sum == (parts + 1) * total / 3) parts++;
+            temp+=i;
+            if(!ok1 && temp==k) ok1=true;
+            else if(!ok2 && ok1 && temp==2*k) ok2=true;
+            else if(!ok3 && ok1 && ok2 && temp==sum) ok3=true;
         }
-        return parts >= 3;
+        return ok1 && ok2 && ok3;
     }
 };
