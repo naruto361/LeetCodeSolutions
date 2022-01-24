664e06f3b82bb85a3186e35f1d8d60463cbf98ee
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2022-01-24 09:02:47 +0530

    520

--- DetectCapital.cpp
+++ DetectCapital.cpp
@@ -12,3 +12,29 @@ public:
         return 1;
     }
 };
+//////////////////////////////////////////////////////////////////
+bool detectCapitalUse(string word) {
+        int l=word.length();
+        if(l==1) return true;
+        string s1,s2,s3;
+        for(int i=0;i<l;i++)
+        {
+            char small,big;
+            small=word[i];big=word[i];
+            if(word[i]>='a' && word[i]<='z')
+            {
+                big=char(word[i]-'a'+'A');
+            }
+            else
+            {
+                small=char(word[i]-'A'+'a');
+            }
+            
+            s1+=big;
+            s2+=small;
+            if(i==0) s3+=big;
+            else s3+=small;
+            
+        }
+        return (word==s1 || word==s2 || word==s3);
+    }
