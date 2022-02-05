a4c4c0a5c82f860a58606439dcfc3f2c5bacef44
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2022-02-05 13:24:27 +0530

    23 HARD

--- MergeKSortedLists.cpp
+++ MergeKSortedLists.cpp
@@ -1,13 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+************1*****************
+class Solution {
+public:
+    
+    // Time Complexity - O(kN)
+    ListNode* merge(ListNode* l1,ListNode* l2)
+    {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        ListNode* head = (l1->val <=l2->val) ? l1:l2;
+        if(l1->val <= l2->val) head->next = merge(l1->next,l2);
+        else head->next = merge(l1,l2->next);
+        return head;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        for(int i=1;i<lists.size();i++)
+        {
+            lists[0]=merge(lists[0],lists[i]);
+        }
+        return lists[0];
+    }
+};
+
+************2******************
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
