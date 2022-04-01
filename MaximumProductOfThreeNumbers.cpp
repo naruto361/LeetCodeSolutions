53dd353a531070128828697b8f86e31b6fd154c4
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2022-04-01 10:00:15 +0530

    628

--- MaximumProductOfThreeNumbers.cpp
+++ MaximumProductOfThreeNumbers.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),greater<int>());
-        vector< int> k(3);
-        k[0]= nums[0]*nums[1]*nums[2];
-        k[1]=nums[0]*nums[1]*nums[nums.size()-1];
-        k[2]=nums[0]*nums[nums.size()-1]*nums[nums.size()-2];
-        return *max_element(k.begin(),k.end());
+        class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        // 3 largest , 2 smallest
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // return max({nums[n-1]*nums[n-2]*nums[n-3] , nums[0]*nums[1]*nums[n-1]});
+        
+        int max1=INT_MIN,max2=INT_MIN,max3=INT_MIN;
+        int min1=INT_MAX,min2=INT_MAX;
+        for(int &i:nums)
+        {
+            if(i>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=i;
+            }
+            else if(i>max2)
+            {
+                max3=max2;
+                max2=i;
+            }
+            else if(i>max3) max3=i;
+            
+            if(i<min1)
+            {
+                min2=min1;
+                min1=i;
+            }
+            else if(i<min2)
+            {
+                min2=i;
+            }
+        }
+        return max(max1*max2*max3 , min1*min2*max1);
+    }
+};
     }
 };
