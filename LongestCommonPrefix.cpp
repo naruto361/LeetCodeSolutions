4e0bdc75a4e38c8fdcbff21aee242f0ddecf5318
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2022-04-07 10:55:07 +0530

    14

--- LongestCommonPrefix.cpp
+++ LongestCommonPrefix.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        if(strs.size()==1) return strs[0];
+        string s1=strs[0] , s2=strs[strs.size()-1];
+        int l=min(s1.length(),s2.length());
+        int i=0;
+        string ans;
+        while(i<l)
+        {
+            if(s1[i]==s2[i])
+            {
+                ans += s1[i];
+            }
+            else return ans;
+            i++;
+        }
+        return ans;
+    }
+};
+
+*********************************************************************
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
         if(strs.size()==0) return "";
         string ans="";
         int mini=INT_MAX;
