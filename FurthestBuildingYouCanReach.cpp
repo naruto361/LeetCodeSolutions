45f37f71b8f4664c4e76717a07490ab8e99df990
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2022-06-21 09:35:52 +0530

    Update FurthestBuildingYouCanReach.cpp

--- FurthestBuildingYouCanReach.cpp
+++ FurthestBuildingYouCanReach.cpp
@@ -1,14 +1,20 @@
-int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        multiset<int> m;
-        for(int i=1 ; i<heights.size() ; i++){
-            if(heights[i]>heights[i-1]){
-                m.insert(heights[i]-heights[i-1]);
-            }
-            if(m.size()>ladders){
-                bricks-=*m.begin();
-                m.erase(m.begin());
-                if(bricks<0)return i-1;
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int,vector<int>,greater<>> pq;
+        for(int i=1;i<heights.size();i++)
+        {
+            if(heights[i] > heights[i-1])
+            {
+                pq.push({heights[i] - heights[i-1]});
+                if(pq.size()>ladders)
+                {
+                    if(pq.top()>bricks) return i-1;
+                    bricks -= pq.top();
+                    pq.pop();
+                }
             }
         }
         return heights.size()-1;
     }
+};
