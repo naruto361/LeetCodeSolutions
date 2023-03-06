42a351cf004a64f6de75d9ce236104c3257a6505
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2023-03-06 11:23:03 +0530

    Update KthMissingPositiveNumber.cpp

--- KthMissingPositiveNumber.cpp
+++ KthMissingPositiveNumber.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
+        int l=0,h=arr.size()-1;
+        while(l<=h) {
+            int mid = l+(h-l)/2;
+            if(arr[mid]-(mid+1)<k) l=mid+1;
+            else h=mid-1;
+        }
+        return l+k;
+    }
+};
+//////////////////////////////////////////////////////////
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
         int f[2001]={0};
         for(int i=0;i<arr.size();i++)
         {
